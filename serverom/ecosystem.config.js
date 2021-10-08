fc448a19af7906864268f791165d7aa3752ad90f
Виталий Лёвкин <unf.hwm@ya.ru>
2021-10-08 23:05:38 +0700

    ci(serverom): fix repo url

--- serverom/ecosystem.config.js
+++ serverom/ecosystem.config.js
@@ -14,9 +14,8 @@ module.exports = {
       user: 'ubuntu',
       host: '3.68.13.118',
       key: 'deploy.key',
-      // key: '~/.ssh/gh-actions-deploy-key.pem',
       ref: 'origin/master',
-      repo: 'https://github.com/roamiiing/om',
+      repo: 'https://github.com/talkiiing/om',
       path: '/home/ubuntu/apps/om',
       'post-deploy':
         'cd serverio && npm ci && pwd && npm run build && pm2 reload ecosystem.config.js --env production && pm2 save',
