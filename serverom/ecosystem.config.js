1387007a09b5075493ea1ce655f6aeee0feb5239
Виталий Лёвкин <unf.hwm@ya.ru>
2021-10-08 22:29:51 +0700

    ci(serverom): deploy fix

--- serverom/ecosystem.config.js
+++ serverom/ecosystem.config.js
@@ -14,9 +14,10 @@ module.exports = {
       user: 'ubuntu',
       host: '3.68.13.118',
       key: 'deploy.key',
+      // key: '~/.ssh/gh-actions-deploy-key.pem',
       ref: 'origin/master',
-      repo: 'https://github.com/roamiiing/liftnow',
-      path: '/home/ubuntu/apps/liftnow',
+      repo: 'https://github.com/roamiiing/om',
+      path: '/home/ubuntu/apps/om',
       'post-deploy':
         'cd serverio && npm ci && pwd && npm run build && pm2 reload ecosystem.config.js --env production && pm2 save',
     },
