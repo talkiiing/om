38366d94cafc1cd3b54233e0a5d264956684adf9
Виталий Лёвкин <unf.hwm@ya.ru>
2021-10-08 22:22:14 +0700

    ci(serverom): deploy

---
+++ serverom/ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: 'www-gateway',
+      script: './dist/apps/www/main.js',
+      instances: 1,
+      env_production: {
+        PORT: 1337,
+      },
+    },
+  ],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: '3.68.13.118',
+      key: 'deploy.key',
+      ref: 'origin/master',
+      repo: 'https://github.com/roamiiing/liftnow',
+      path: '/home/ubuntu/apps/liftnow',
+      'post-deploy':
+        'cd serverio && npm ci && pwd && npm run build && pm2 reload ecosystem.config.js --env production && pm2 save',
+    },
+  },
+};
